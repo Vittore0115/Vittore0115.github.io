23c8eb39b61a24eda7e7bd15913707b25725e402
zwt <1607458660@qq.com>
2021-03-29 10:25:43 +0800

    Site updated: 2021-03-29 10:25:42

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -4,8 +4,7 @@ $(document).ready(function () {
 
   $(document).trigger('bootstrap:before');
 
-  // NexT.utils.isMobile() && window.FastClick.attach(document.body);
-  NexT.utils.isMobile()
+  NexT.utils.isMobile() && window.FastClick.attach(document.body);
   NexT.utils.lazyLoadPostsImages();
 
   NexT.utils.registerESCKeyEvent();
